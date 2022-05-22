86c62f87d237b077b800624b51ebb5ead7ec0e98
Mike Rhodes <mike.rhodes@gmail.com>
2022-05-22 19:04:27 +0100

    Add some kinda helpful comments; hard to explain the alg

--- frog.go
+++ frog.go
@@ -1,3 +1,32 @@
+// This solution to the froggy problem takes the approach
+// of setting up states dangling off each letter. As we
+// progress across the river, see where we can get to with
+// our next set of hops. Store the number of hops as a
+// possible "state" at that location. At each location,
+// process all the states we find there (i.e., each of
+// the prior number of hops we took to get there).
+//
+// Here's the states we have at each point for the
+// spaces further on than where we are looking at:
+//  v
+// "***~~*"
+//  1
+//   v
+// "***~~*"
+//   12     <- In the previous step, you could hop 1 or 2
+//             forward, so you get 1 in the
+//             2nd location and 2 in the third location.
+//    v
+// "***~~*"
+//    22   <-- that second 2 is actually invalid, because it's in the river
+//    1
+//       v
+// "***~~*"
+//       3 <-- you have to jump 3 from state 2, to not fall
+//
+// "***~~*"
+//           4  <- longest hop takes you off the end of the river
+
 package froggy
 
 func froggy(input []byte) bool {
@@ -17,10 +46,10 @@ func froggy(input []byte) bool {
 		}
 
 		for _, state := range states[i] {
-			// Test whether the largest of the possible numbers
-			// of hops from this location will take us
-			// off the end of the river. This is our exit condition,
-			// and if we never hit it, we can't find an answer.
+			// Test whether the largest possible hop from this
+			// location will take us off the end of the river.
+			// This is our exit condition, and if we never hit
+			// it, we can't find an answer.
 			if i+state+1 >= len(input) {
 				return true
 			}
@@ -31,6 +60,11 @@ func froggy(input []byte) bool {
 			for _, hops := range []int{state - 1, state, state + 1} {
 				if hops > 0 { // state might be 1
 					loc := i + hops
+					// there's a question here of how likely
+					// we are to end up with duplicates by
+					// not checking for whether we already have
+					// this state. And if we don't, we'll just
+					// amplify that as we progress.
 					states[loc] = append(states[loc], hops)
 				}
 			}
